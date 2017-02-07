ab29e1c2abb1f220dd6d1937899aa093b3a511bf
Jesse Shieh <jesse@gigalixir.com>
2017-02-07 11:20:24 -0800

    fix typo.

--- lib/tasks.ex
+++ lib/tasks.ex
@@ -2,7 +2,7 @@ defmodule Tasks do
   def migrate do
     {:ok, _} = Application.ensure_all_started(:gigalixir_getting_started)
 
-    path = Application.app_dir(:web, "priv/repo/migrations")
+    path = Application.app_dir(:gigalixir_getting_started, "priv/repo/migrations")
 
     Ecto.Migrator.run(GigalixirGettingStarted.Repo, path, :up, all: true)
   end
