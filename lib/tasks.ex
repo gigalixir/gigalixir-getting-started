28d796b2d638a03fef225c72f1c92901dfbfd0a0
Jesse Shieh <jesse@gigalixir.com>
2017-02-07 11:42:40 -0800

    stop after migration is done.

--- lib/tasks.ex
+++ lib/tasks.ex
@@ -5,5 +5,7 @@ defmodule Tasks do
     path = Application.app_dir(:gigalixir_getting_started, "priv/repo/migrations")
 
     Ecto.Migrator.run(GigalixirGettingStarted.Repo, path, :up, all: true)
+
+    :init.stop()
   end
 end  
