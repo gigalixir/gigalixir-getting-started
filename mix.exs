3539e8d829b44e26f5c6623d9d1ceaeaa5726eee
Jesse <jesse@gigalixir.com>
2017-08-08 14:11:37 -0700

    oops, transitive dependency.

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule GigalixirGettingStarted.Mixfile do
   def application do
     [mod: {GigalixirGettingStarted, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :libcluster, :ssl, :runtime_tools, :pdf_generator]]
+                    :phoenix_ecto, :postgrex, :libcluster, :ssl, :runtime_tools, :pdf_generator, :misc_random]]
   end
 
   # Specifies which paths to compile per environment.
