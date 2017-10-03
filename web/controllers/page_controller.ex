753809ef6936c0fd213be861be1493c3e452374a
Jesse <jesse@gigalixir.com>
2017-10-03 11:37:10 -0700

    endpoint to list nodes.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,10 @@ defmodule GigalixirGettingStarted.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def nodes(conn, params) do
+    json conn, %{
+      data: Node.list ++ Node.self
+    }
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule GigalixirGettingStarted.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/nodes", PageController, :nodes
   end
 
   # Other scopes may use custom stacks.
