75334b8f03c56d5e83f8fa243d995d6e50137545
Jesse <jesse@gigalixir.com>
2018-03-22 13:41:48 -0700

    v2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GigalixirGettingStarted.Mixfile do
   def project do
     [
       app: :gigalixir_getting_started,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
