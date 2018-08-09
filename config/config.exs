de6d286af0862f6bd6dc17e78d4960ef5d3012fe
Jesse <jesse@gigalixir.com>
2018-08-08 17:03:09 -0700

    mix phx.new

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :gigalixir_getting_started,
+  ecto_repos: [GigalixirGettingStarted.Repo]
+
+# Configures the endpoint
+config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "2Co15w0D1EBC810DwTtcsu//jcRaJ3JFqrf522NibO0ccILjRK9C8pzzugJZQI2j",
+  render_errors: [view: GigalixirGettingStartedWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: GigalixirGettingStarted.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:user_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
