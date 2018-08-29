9b0464e3861bf5f9654689795b1251b110a110c9
Jesse <jesse@gigalixir.com>
2018-08-29 09:54:05 -0700

    configure gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -58,7 +58,18 @@ config :logger, level: :info
 #
 #     config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint, server: true
 #
+config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint,
+  load_from_system_env: true,
+  # http: [port: {:system, "PORT"}], # Uncomment this line if you are running Phoenix 1.2
+  server: true, # Without this line, your app will not start the web server!
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "example.com", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+config :gigalixir_getting_started, GigalixirGettingStarted.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  ssl: true,
+  pool_size: 1 # Free tier db only allows 1 connection
+
