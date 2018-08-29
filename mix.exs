001747c354a635b20916d761ad97cd59f6dea716
Jesse <jesse@gigalixir.com>
2018-08-29 09:51:55 -0700

    install distilery 2.0

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule GigalixirGettingStarted.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
+      {:distillery, "~> 2.0.0"},
       {:cowboy, "~> 1.0"}
     ]
   end
