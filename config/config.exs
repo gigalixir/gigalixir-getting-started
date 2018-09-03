e71ec3e25145d1eea6fafb0d16529df6d5ecf7a2
Jesse <jesse@gigalixir.com>
2018-09-03 09:16:54 -0700

    mix phx.new

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :gigalixir_getting_started,
+  ecto_repos: [GigalixirGettingStarted.Repo]
+
+# Configures the endpoint
+config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "C5NOEEEdSN0tA6z/EflQQA/pzCSecU6z4mljH2+mN00vay0l6MNiHFEAMeaVA9AM",
+  render_errors: [view: GigalixirGettingStartedWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: GigalixirGettingStarted.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:user_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ priv/repo/seeds.exs
@@ -0,0 +1,11 @@
+# Script for populating the database. You can run it as:
+#
+#     mix run priv/repo/seeds.exs
+#
+# Inside the script, you can read and write to any of your
+# repositories directly:
+#
+#     GigalixirGettingStarted.Repo.insert!(%GigalixirGettingStarted.SomeSchema{})
+#
+# We recommend using the bang functions (`insert!`, `update!`
+# and so on) as they will fail if something goes wrong.
