47a370df8f17f8368be7675eb12b4d77d26dcf4f
Jesse <jesse@gigalixir.com>
2018-12-17 16:27:27 -0800

    configure.

--- config/prod.exs
+++ config/prod.exs
@@ -68,4 +68,15 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
+#
+config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint,
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :gigalixir_getting_started, GigalixirGettingStarted.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections.
+
