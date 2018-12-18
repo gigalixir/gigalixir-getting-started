a4fe072811e9b98581b5d5ea61df008dd9937a41
Jesse <jesse@gigalixir.com>
2018-12-17 16:30:43 -0800

    define elixir and erlang versions. print node name to try and isolate issues.

--- lib/gigalixir_getting_started_web/controllers/page_controller.ex
+++ lib/gigalixir_getting_started_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule GigalixirGettingStartedWeb.PageController do
   use GigalixirGettingStartedWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    json conn, %{node: "#{inspect Node.self}"}
+    # render(conn, "index.html")
   end
 end
