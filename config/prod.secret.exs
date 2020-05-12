a018ba71d2dcaa6449efe2acd9fae9a208ee2122
Jesse <jesse@gigalixir.com>
2020-05-12 10:44:26 -0700

    lower pool size for free db.

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -14,7 +14,7 @@ database_url =
 config :gigalixir_getting_started, GigalixirGettingStarted.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
