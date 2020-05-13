d0ba5eb864df51d40e7c6eab51d378813f3ace7a
Jesse <jesse@gigalixir.com>
2020-05-13 07:32:34 -0700

    copy config from distillery.

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,22 @@
 use Mix.Config
 
+config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint,
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  server: true, # Without this line, your app will not start the web server!
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  version: Mix.Project.config[:version] # to bust cache during hot upgrades
+
+config :gigalixir_getting_started, GigalixirGettingStarted.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+
+
 # For production, don't forget to configure the url host
 # to something meaningful, Phoenix uses this information
 # when generating URLs.
