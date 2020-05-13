058c047a198fcd90bd65cc2db25d6bb12f054562
Jesse <jesse@gigalixir.com>
2020-05-13 07:20:00 -0700

    install distillery

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule GigalixirGettingStarted.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:distillery, "~> 2.1"},
       {:phoenix, "~> 1.5.1"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
