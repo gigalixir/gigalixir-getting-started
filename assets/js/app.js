429d3badc1dc550341249fadbc97b86acbb46c00
Jesse <jesse@gigalixir.com>
2021-09-10 20:39:35 -0400

    actually use alpine.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
+import Alpine from 'alpinejs' 
+window.Alpine = Alpine 
+Alpine.start()
+
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
 import "../css/app.css"
