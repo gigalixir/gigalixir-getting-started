b965332464d8c464a2999e3ae2938dde5bf92531
Tim <tim@gigalixir.com>
2023-06-16 09:39:57 -0500

    chore: updates for phoenix 1.7.6

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule GigalixirGettingStarted.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.3"},
+      {:phoenix, "~> 1.7.6"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
