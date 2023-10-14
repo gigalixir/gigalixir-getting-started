b86d548438df6ba7d460083856f6d1101e7d6968
Tim <tim@gigalixir.com>
2023-10-14 07:07:42 -0500

    feat: Elixir 1.15.7

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -8,8 +8,8 @@ const path = require("path")
 module.exports = {
   content: [
     "./js/**/*.js",
-    "../lib/*_web.ex",
-    "../lib/*_web/**/*.*ex"
+    "../lib/gigalixir_getting_started_web.ex",
+    "../lib/gigalixir_getting_started_web/**/*.*ex"
   ],
   theme: {
     extend: {
